d1daa32425c42944418a8bc3b1763219252e872f
jameshskelton <jameshskelton@gmail.com>
2023-07-12 17:56:48 -0500

    added settings for Gradient demo

--- lora/train_lora.sh
+++ lora/train_lora.sh
@@ -1,14 +1,14 @@
-export SAMPLE_DIR="lora/samples/sculpture"
-export OUTPUT_DIR="lora/lora_ckpt/sculpture_lora"
+export SAMPLE_DIR="/notebooks/lora/test/"
+export OUTPUT_DIR="lora/lora_ckpt/log/"
 
-export MODEL_NAME="runwayml/stable-diffusion-v1-5"
+export MODEL_NAME="/datasets/stable-diffusion-diffusers/stable-diffusion-v1-5/"
 export LORA_RANK=16
 
 accelerate launch lora/train_dreambooth_lora.py \
   --pretrained_model_name_or_path=$MODEL_NAME  \
   --instance_data_dir=$SAMPLE_DIR \
   --output_dir=$OUTPUT_DIR \
-  --instance_prompt="a photo of a sculpture" \
+  --instance_prompt="a retrofuturistic comic book artwork of a man firing a laser gun at a large robot" \
   --resolution=512 \
   --train_batch_size=1 \
   --gradient_accumulation_steps=1 \
@@ -16,6 +16,6 @@ accelerate launch lora/train_dreambooth_lora.py \
   --learning_rate=2e-4 \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
-  --max_train_steps=200 \
+  --max_train_steps=100 \
   --lora_rank=$LORA_RANK \
-  --seed="0"
+  --seed="0"   
